ef11e6568ecd66650d324c76d43c15a45e2fe6cf
sandra <mjfelici@gmail.com>
2022-04-27 10:42:40 -0300

    padding-top close

--- addConvidados.js
+++ addConvidados.js
@@ -1,3 +1,4 @@
+// criando lista vazia
 const lista = [];
 
 function listaConvidados() {
@@ -7,25 +8,27 @@ function listaConvidados() {
     const idade = document.querySelector("#idade").value;
     const emailConvidado = email.value;
     const convidados = qtdconvidados.value
-
+        //  criando um objeto
     const pessoa = {
         nome: recebeNome,
         idade: +idade,
         email: emailConvidado
     };
-
+    // validação a idade
     if (pessoa.idade < 18) {
         msgErro.style.display = 'block';
         return;
     }
-
+    // precorrendo a lista para validar a quantidade de convidados
     if (lista.length >= +convidados) {
         msgQuantidadePessoa.style.display = 'flex';
         return;
     }
-
+    // add pessoas na lista
     lista.push(pessoa);
+    // escrevendo na tela quantidade de convidados e criando lista 
     quantidadeConvidado.innerHTML = `No momento sua lista está com [${lista.length}] convidado(s)<br>
+    
     <ol>
             ${lista.map(p => {
                 return `
